b87594c506ebe97aee771dd5f702080cd9b8905c
Ankit singh <2023pcecsankit019@poornima.org>
2025-06-26 17:28:25 +0530

    Update Day1.cpp
    Second Largest:
    Given an array of positive integers arr[], return the second largest element from the array.
    If the second largest element doesn't exist then return -1.
    
    Note: The second largest element should not be equal to the largest element.

---
+++ Arrays/Day1.cpp
@@ -0,0 +1,27 @@
+/* Second Largest:
+Given an array of positive integers arr[], return the second largest element from the array.
+If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element. */
+
+
+int getSecondLargest(int *arr, int n) {
+    // code here
+    if(n<2)
+        return -1;
+    
+    int first = -1,second = -1;    
+        
+    for(int i=0; i<n; i++){
+        if(arr[i] > first){
+            second = first;
+            first = arr[i];
+        }
+        else if (arr[i] > second && arr[i] < first){
+            second = arr[i];
+        }
+            
+    }    
+    
+    return second;
+}
