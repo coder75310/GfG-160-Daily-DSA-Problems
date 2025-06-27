2209c4331373ad039350bece918db3fb58cc879f
Ankit singh <2023pcecsankit019@poornima.org>
2025-06-27 17:06:03 +0530

    Update Day2.cpp

--- Arrays/Day2.cpp
+++ Arrays/Day2.cpp
@@ -1,3 +1,9 @@
+/* Move All Zeroes to End:
+You are given an array arr[] of non-negative integers.
+Your task is to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements.
+The operation must be performed in place, meaning you should not use extra space for another array. */
+
+
 // User function template for C++
 class Solution {
   public:
