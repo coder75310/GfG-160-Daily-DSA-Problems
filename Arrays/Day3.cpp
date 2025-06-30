fa5e4c389ac3db69d8c4c0a8d04b051c92a994a5
Ankit singh <2023pcecsankit019@poornima.org>
2025-06-30 11:44:36 +0530

    Update Day3.cpp

--- Arrays/Day3.cpp
+++ Arrays/Day3.cpp
@@ -1,3 +1,8 @@
+/* Reverse an Array :
+You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Note: Modify the array in place. */
+
 class Solution {
   public:
     void reverseArray(vector<int> &arr) {
