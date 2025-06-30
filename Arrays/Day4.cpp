7a23a377cfbf775c85d1fad30c3b3f57d92a039c
Ankit singh <2023pcecsankit019@poornima.org>
2025-06-30 11:43:21 +0530

    Update Day4.cpp

--- Arrays/Day4.cpp
+++ Arrays/Day4.cpp
@@ -1,3 +1,9 @@
+/* Rotate Array :
+Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. 
+Do the mentioned change in the array in place.
+
+Note: Consider the array as circular. */
+
 class Solution {
   public:
 
