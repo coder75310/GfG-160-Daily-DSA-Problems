f667fd01121c4d7b469875c4ac6a70e96fb89da0
Ankit singh <2023pcecsankit019@poornima.org>
2025-06-30 11:40:39 +0530

    Update Day5.cpp

--- Arrays/Day5.cpp
+++ Arrays/Day5.cpp
@@ -1,3 +1,10 @@
+/* Next Permutation : 
+Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers 
+into the lexicographically next greater permutation. 
+If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
+
+Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order. */
+
 class Solution {
   public:
     void nextPermutation(vector<int>& arr) {
