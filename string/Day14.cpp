16b7d326659506a42b91b57af741de157ae2e628
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-08 13:02:29 +0530

    Day14.cpp
    Implement Atoi

---
+++ string/Day14.cpp
@@ -0,0 +1,45 @@
+/* Implement Atoi :
+Given a string s, the objective is to convert it into integer format without utilizing any built-in functions. Refer the below steps to know about atoi() function.
+
+Cases for atoi() conversion:
+
+1. Skip any leading whitespaces.
+2. Check for a sign (‘+’ or ‘-‘), default to positive if no sign is present.
+3. Read the integer by ignoring leading zeros until a non-digit character is encountered or end of the string is reached. If no digits are present, return 0.
+4. If the integer is greater than 231 – 1, then return 231 – 1 and if the integer is smaller than -231, then return -231. */
+
+
+class Solution {
+  public:
+    int myAtoi(char *s) {
+        // Your code here
+        int i = 0;
+        long result = 0;
+        int sign = 1;
+        
+        // 1. Skip leading whitespaces
+        while(s[i] == ' '){
+            i++;
+        }
+        
+        // 2. Check for sign
+        if(s[i] == '+' || s[i] == '-'){
+            if(s[i] == '-') sign = -1;
+            i++;
+        }
+        
+        // 3. Convert digits to integer
+        while(s[i] >= '0' && s[i] <= '9'){
+            int digit = s[i] - '0';
+            result = result * 10 + digit;
+            
+        // 4. Check for Overflow
+        if(sign == 1 && result > INT_MAX) return INT_MAX;
+        if (sign == -1 && -result < INT_MIN) return INT_MIN;
+        
+            i++;
+        }
+        
+        return (int)(sign * result);
+    }
+};
