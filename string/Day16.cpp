7b8c29ce76f0c942631629437c0a9b073a1bb5cd
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-10 21:57:28 +0530

    Update Day16.cpp

--- string/Day16.cpp
+++ string/Day16.cpp
@@ -2,7 +2,7 @@
 Given two non-empty strings s1 and s2, consisting only of lowercase English letters, determine whether they are anagrams of each other or not. 
 Two strings are considered anagrams if they contain the same characters with exactly the same frequencies, regardless of their order. */
 
-
+////////////////////////////
 class Solution {
   public:
     bool areAnagrams(string& s1, string& s2) {
@@ -25,7 +25,7 @@ class Solution {
         for(int i=0; i<26; ++i){
             if(freq[i] != 0)
                 return false;
-        } //
+        } 
             
             return true;
         }
