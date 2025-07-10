a2a16272127ff90bfc693e5f1f3168304013e0af
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-10 21:56:30 +0530

    Create Day16.cpp
    Anagram

---
+++ string/Day16.cpp
@@ -0,0 +1,33 @@
+/* Anagram :
+Given two non-empty strings s1 and s2, consisting only of lowercase English letters, determine whether they are anagrams of each other or not. 
+Two strings are considered anagrams if they contain the same characters with exactly the same frequencies, regardless of their order. */
+
+
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        // code here
+        if(s1.length() != s2.length()) 
+            return false;
+            
+        int freq[26] = {0};
+        
+        // Count frequency for s1
+        for(char c : s1){
+            freq[c - 'a']++;
+        }
+        
+        // Subtract frequency for s2
+        for(char c : s2){
+            freq[c - 'a']--;
+        }
+        
+        // Check if all counts are 0
+        for(int i=0; i<26; ++i){
+            if(freq[i] != 0)
+                return false;
+        }
+            
+            return true;
+        }
+};
