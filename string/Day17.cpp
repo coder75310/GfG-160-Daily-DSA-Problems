dc1f179a2ca8ee53793ae7b3160ebe65b5b2d8c0
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-11 20:39:10 +0530

    Create Day17.cpp

---
+++ string/Day17.cpp
@@ -0,0 +1,29 @@
+/* Non Repeating Character :
+Given a string s consisting of lowercase English Letters. 
+Return the first non-repeating character in s.
+If there is no non-repeating character, return '$'.
+Note: When you return '$' driver code will output -1. */
+
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        int freq[26] = {0};
+        
+        // First pass: count frequency
+        for(char ch : s){
+            freq[ch - 'a']++;
+        }
+        
+        // Second pass: find first non-repeating character
+        for(char ch : s){
+            if (freq[ch - 'a'] == 1){
+                return ch;
+            }
+        }
+        
+        // If none found, return '$'
+        return '$';
+    }
+};
