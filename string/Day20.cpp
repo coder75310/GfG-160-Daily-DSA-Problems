17d8c78606323788288809ae44ae8e0feb25060d
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-14 20:44:41 +0530

    Create Day20.cpp

---
+++ string/Day20.cpp
@@ -0,0 +1,21 @@
+/* Strings Rotations of Each Other :
+You are given two strings s1 and s2, of equal lengths. The task is to check if s2 is a rotated version of the string s1.
+
+Note: A string is a rotation of another if it can be formed by moving characters from the start to the end (or vice versa) without rearranging them. */
+
+
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
+        // Your code here
+        // Step 1: If Lengths are not equal, s2 can't be a rotation of s1
+        if(s1.length() != s2.length()) return false;
+        
+        // step 2: Concatenate s1 with itself
+        string temp = s1 + s1;
+        
+        return temp.find(s2) != string::npos;
+    }
+};
