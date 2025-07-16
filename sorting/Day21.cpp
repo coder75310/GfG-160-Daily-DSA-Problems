dd147e874013f3ba1ea7bc52e8b21c71ee26736f
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-16 17:32:07 +0530

    Create Day21.cpp

---
+++ sorting/Day21.cpp
@@ -0,0 +1,26 @@
+/* Sort 0s, 1s and 2s :
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+You need to solve this problem without utilizing the built-in sort function. */
+
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int low = 0, mid = 0, high = arr.size() - 1;
+        
+        while(mid <= high){
+            if(arr[mid] == 0){
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1){
+                mid++;
+            } else { // arr[mid] == 2
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
