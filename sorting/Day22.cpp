77192fb4b9c8b4c4bd56de9466acbbd9c4fa4232
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-17 11:13:18 +0530

    Create Day22.cpp

---
+++ sorting/Day22.cpp
@@ -0,0 +1,24 @@
+/* Find H-Index :
+Given an integer array citations[], where citations[i] represents the number of citations for the ith paper, your have to compute the H-index of the researcher.
+The H-index is defined as the maximum value H such that the researcher has at least H papers with at least H citations each.
+ */
+
+
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find hIndex
+    int hIndex(vector<int>& citations) {
+        // code here
+        sort(citations.begin(), citations.end(), greater<int>());
+        int h = 0;
+        for(int i = 0;i < citations.size(); i++){
+            if(citations[i] >= i + 1){
+                h = i + 1;
+            } else {
+                break;
+            }
+        }
+        return h;
+    }
+};
