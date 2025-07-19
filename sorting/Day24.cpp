f91e4aaefa3bbb50093e527af8c5761244f8e6db
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-19 18:58:31 +0530

    Create Day24.cpp

---
+++ sorting/Day24.cpp
@@ -0,0 +1,26 @@
+/* Overlapping Intervals :
+Given an array of Intervals arr[][], where arr[i] = [starti, endi]. The task is to merge all of the overlapping Intervals. */
+
+
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        // Code here
+        // step 1: Sort intervals based on the starting time
+        sort(arr.begin(), arr.end());
+        
+        vector<vector<int>> merged;
+        
+        for(const auto& interval : arr){
+            // If merged is empty or no overlap,add the interval
+            if(merged.empty() || merged.back()[1] < interval[0]){
+                merged.push_back(interval);
+            } else {
+            // Overlap: merge current interval with previous
+                merged.back()[1] = max(merged.back()[1], interval[1]);
+            }
+        }
+        
+        return merged;
+    }
+};
