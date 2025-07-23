aa6f3a92afc1f5ac06f86b7d7a12674a5f3cbb6f
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-23 10:34:06 +0530

    Update Day27.cpp

--- sorting/Day27.cpp
+++ sorting/Day27.cpp
@@ -5,7 +5,7 @@ Modify a[] so that it contains the first n elements and modify b[] so that it co
 
 class Solution {
   public:
-  // Write
+  // Wri
     void mergeArrays(vector<int>& a, vector<int>& b) {
         // code here
         int n = a.size(), m = b.size();
