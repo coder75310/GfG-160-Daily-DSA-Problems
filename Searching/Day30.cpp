b07e66586ab8a48bbc937816c4b7d57d05b894df
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-26 17:14:44 +0530

    Update Day30.cpp

--- Searching/Day30.cpp
+++ Searching/Day30.cpp
@@ -5,7 +5,7 @@ If the key is not present in the array, return -1. */
 
 class Solution {
   public:
-  // write your code he
+  // write your code 
     int search(vector<int>& arr, int key) {
         // Code Here
         int low = 0, high = arr.size() - 1;
