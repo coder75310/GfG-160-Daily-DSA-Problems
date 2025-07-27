2ab33f28e7483e5f21ce249ffbc47b8767aa7f05
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-27 11:43:27 +0530

    Update Day31.cpp

--- Searching/Day31.cpp
+++ Searching/Day31.cpp
@@ -9,7 +9,7 @@ Note: Consider the element before the first element and the element after the la
 
 
 //  Function to find the peak element
-// yo
+// 
 int peakElement(int *arr, int n) {
     // code here
     int low = 0, high = n - 1;
