318a9010adf33fd44e9fd7d0a5da26e91a66bc47
Ankit singh <2023pcecsankit019@poornima.org>
2025-07-30 19:32:20 +0530

    Create Day33.cpp

---
+++ Searching/Day33.cpp
@@ -0,0 +1,49 @@
+/* Aggressive Cows :
+You are given an array with unique elements of stalls[], which denote the positions of stalls. 
+You are also given an integer k which denotes the number of aggressive cows. 
+The task is to assign stalls to k cows such that the minimum distance between any two of them is the maximum possible. */
+
+
+// User function Template for C
+
+// Comparision function for qsort
+int compare(const void *a, const void *b){
+    return (*(int*)a - *(int*)b);
+}
+
+// bool isPossible(int stalls[], int n, int k, int mid) {
+int isPossible(int stalls[], int n, int k, int mid) {
+    int count = 1;
+    int last_pos = stalls[0];
+    
+    for(int i = 1; i < n; i++){
+        if(stalls[i] - last_pos >= mid) {
+            count++;
+            last_pos = stalls[i];
+        }
+    }
+    
+    return count >= k;
+}
+
+int aggressiveCows(int *stalls, int n, int k) {
+    // code here
+    qsort(stalls, n, sizeof(int), compare);
+    
+    int low = 1;
+    int high = stalls[n - 1] - stalls[0];
+    int ans = 0;
+    
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (isPossible(stalls, n, k, mid)) {
+            ans = mid;      // valid, try for more
+            low = mid  + 1;
+        } else {
+            high = mid - 1;     // Not possible, reduce distance
+        }
+    }
+    
+    return ans;
+}
