271b2a852f3c2113cd9deaaa0fa2bbe8e4d17702
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-05 11:38:27 +0530

    Update Day38.cpp

--- Matrix/Day38.cpp
+++ Matrix/Day38.cpp
@@ -1,1 +1,28 @@
+class Solution{
+    public:
+    void rotateMatrix(vector<vector<int>>& mat){
+        // code here
+        int n=mat.size();
+        // Transpose the matrix
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        
+        // reverse column
+        for(int j=0; j<n; j++){
+            for(int i=0, k=n-1; i<k; i++, k--){
+                swap(mat[i][j], mat[k][j]);
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
 
