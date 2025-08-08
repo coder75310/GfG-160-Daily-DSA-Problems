e5fb9a09b6dbb386beb6d6cc1310eb1ae47ecf25
Ankit Singh <2023pcecsankit019@poornima.org>
2025-08-08 19:35:45 +0530

    Update Day41.cpp

--- Matrix/Day41.cpp
+++ Matrix/Day41.cpp
@@ -1,5 +1,6 @@
 Set Matrix Zeros :
-You are given a 2D matrix mat[][] of size n x m. The task is to modify the matrix such that if mat[i][j] is 0, all the elements in the i-th row and j-th column are set to 0.
+You are given a 2D matrix mat[][] of size n x m. The task is to modify the matrix such that if mat[i][j] is 0, 
+all the elements in the i-th row and j-th column are set to 0.
 // class Solution {
 //   public:
 //     void setMatrixZeroes(vector<vector<int>> &mat) {
